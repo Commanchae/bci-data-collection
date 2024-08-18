24289580a85382da64a038e146f47d84fb3fd6de
Taylor <58658294+Commanchae@users.noreply.github.com>
2024-08-18 18:48:43 +1000

    Created .bat files to help activate local virtual environments.

---
+++ activate-venv.bat
@@ -0,0 +1,13 @@
+cd bci-data-collection-venv\Scripts
+call activate
+cd ../..
+cls
+
+if "%1" == "False" (
+echo 
+cls	
+) else (
+cmd /k
+)
+
+
---
+++ run-streamlit.bat
@@ -0,0 +1,4 @@
+set linger=False
+call activate-venv.bat %linger%
+streamlit run main.py
+cmd \k
